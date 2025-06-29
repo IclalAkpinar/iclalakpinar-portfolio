f14019d37f7d0a74ffd314fd241e14773ec4426a
IclalAkpinar <iclalakpinar2404@gmail.com>
2025-06-29 18:30:11 +0300

    update pb for achivement card

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -3,7 +3,7 @@ import { SectionHeading } from "./SectionHeading";
 
 export function Achievements() {
   return (
-    <section className="flex items-center justify-center w-full pb-12">
+    <section className="flex items-center justify-center w-full pb-20">
       <div className="w-[90vw] lg:w-[78vw] max-w-7xl mx-auto ">
         <SectionHeading title="Achievements" />
         <AchievementContainer />
